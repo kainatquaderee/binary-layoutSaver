b497473ad321a5438f94562aa0c63b0a04600802
kainatquaderee <68397736+kainatquaderee@users.noreply.github.com>
2022-11-09 13:22:49 +0600

    done job

--- com.pajuelo.plasmaConfSaver/contents/scripts/load.sh
+++ com.pajuelo.plasmaConfSaver/contents/scripts/load.sh
@@ -5,33 +5,41 @@ dataPath=$3
 modelData=$4
 
 if [ -d "$savePath/$modelData/icons" ]; then
+    rm -rf "$dataPath/icons.bak"
     mv "$dataPath/icons" "$dataPath/icons.bak"
     cp -r "$savePath/$modelData/icons" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/color-schemes" ]; then
+    rm -rf "$dataPath/color-schemes.bak"
     mv "$dataPath/color-schemes" "$dataPath/color-schemes.bak"
     cp -r "$savePath/$modelData/color-schemes" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/plasma" ]; then
+    rm -rf "$dataPath/plasma.bak"
     mv "$dataPath/plasma" "$dataPath/plasma.bak"
     cp -r "$savePath/$modelData/plasma" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/wallpapers" ]; then
+    rm -rf "$dataPath/wallpapers.bak"
     mv "$dataPath/wallpapers" "$dataPath/wallpapers.bak"
     cp -r "$savePath/$modelData/wallpapers" "$dataPath"    
 fi
 
 if [ -d "$savePath/$modelData/kfontinst" ]; then
+    rm -rf "$dataPath/kfontinst.bak"
     mv "$dataPath/kfontinst" "$dataPath/kfontinst.bak"
     cp -r "$savePath/$modelData/kfontinst" "$dataPath"    
 fi
 
 #backups
+rm -rf "$configPath/plasma-org.kde.plasma.desktop-appletsrc.bak"
 mv "$configPath/plasma-org.kde.plasma.desktop-appletsrc" "$configPath/plasma-org.kde.plasma.desktop-appletsrc.bak"
+rm -rf "$configPath/.config/plasmarc.bak"
 mv "$configPath/.config/plasmarc" "$configPath/.config/plasmarc.bak"
+rm -rf "$configPath/.config/plasmashellrc.bak"
 mv "$configPath/.config/plasmashellrc" "$configPath/.config/plasmashellrc.bak"
 
 # plasma config files
@@ -44,10 +52,14 @@ cp "$savePath/$modelData/kdeglobals" "$configPath/kdeglobals"
 cp "$savePath/$modelData/kwinrc" "$configPath/kwinrc"
 cp "$savePath/$modelData/kwinrulesrc" "$configPath/kwinrulesrc"
                                     
-#latte-dock config files                                    
+#latte-dock config files   
+rm -rf "$configPath/lattedockrc.bak"
 mv "$configPath/lattedockrc" "$configPath/lattedockrc.bak"
+rm -rf "$configPath/latte.bak"
 mv "$configPath/latte" "$configPath/latte.bak"
+rm -rf "$configPath/autostart.bak"
 mv "$configPath/autostart" "$configPath/autostart.bak"
+rm -rf "$configPath/Kvantum.bak"
 mv "$configPath/Kvantum" "$configPath/Kvantum.bak"
 cp "$savePath/$modelData/lattedockrc" "$configPath/lattedockrc"
 cp -r "$savePath/$modelData/latte" "$configPath"
@@ -90,5 +102,5 @@ fi
 
 qdbus org.kde.KWin /KWin reconfigure 
 konsole -e kquitapp5 plasmashell && kstart5 plasmashell --windowclass plasmashell --window Desktop
-                                     
+kstart5 plasmashell --windowclass plasmashell --window Desktop                                    
                             
